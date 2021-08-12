de96eca955ee051fe064523c8d5963e831b64dc4
Braulio Rodriguez <bdrodriguez.dev@gmail.com>
2021-08-12 19:49:57 -0400

    Dummys

--- todos/dummy-todos.js
+++ todos/dummy-todos.js
@@ -7,75 +7,75 @@ let year = date_ob.getFullYear();
 
 // prints date & time in YYYY-MM-DD format
 const todaysDate = year + "-" + month + "-" + date;
-const dummyTodos = [
-  {
-    todo: "This is your first todo. Congratulations! Feel free to complete me.",
-    dueDate: todaysDate,
-    completed: false,
-    list: "inbox",
-  },
-  {
-    todo: "Water the plants",
-    dueDate: "2021-08-11",
-    completed: true,
-    list: "inbox",
-  },
-];
 // const dummyTodos = [
+//   {
+//     todo: "This is your first todo. Congratulations! Feel free to complete me.",
+//     dueDate: todaysDate,
+//     completed: false,
+//     list: "inbox",
+//   },
 //   {
 //     todo: "Water the plants",
 //     dueDate: "2021-08-11",
 //     completed: true,
-//     list: "home",
-//   },
-//   {
-//     todo: "Beat Gilbert at ping pong",
-//     dueDate: "2021-08-11",
-//     completed: false,
-//     list: "home",
-//   },
-//   {
-//     todo: "Beat Kyle at pong",
-//     dueDate: "2021-08-11",
-//     completed: false,
-//     list: "home",
-//   },
-//   {
-//     todo: "Defeat neoliberalism",
-//     dueDate: "2021-08-11",
-//     completed: false,
 //     list: "inbox",
-//   },
-//   {
-//     todo: "Become awesome",
-//     dueDate: "2021-08-11",
-//     completed: true,
-//     list: "inbox",
-//   },
-//   {
-//     todo: "Study mycology and develop a start up that uses mycellium networks to sequester C02",
-//     dueDate: "2021-08-11",
-//     completed: false,
-//     list: "inbox",
-//   },
-//   {
-//     todo: "Find a dev job",
-//     dueDate: "2021-08-11",
-//     completed: false,
-//     list: "work",
-//   },
-//   {
-//     todo: "Sign up for 401k",
-//     dueDate: "2021-08-11",
-//     completed: false,
-//     list: "work",
-//   },
-//   {
-//     todo: "Meet Nelson for lunch",
-//     dueDate: "2021-08-11",
-//     completed: false,
-//     list: "work",
 //   },
 // ];
+const dummyTodos = [
+  {
+    todo: "Water the plants",
+    dueDate: "2021-08-11",
+    completed: true,
+    list: "home",
+  },
+  {
+    todo: "Beat Gilbert at ping pong",
+    dueDate: "2021-08-11",
+    completed: false,
+    list: "home",
+  },
+  {
+    todo: "Beat Kyle at pong",
+    dueDate: "2021-08-11",
+    completed: false,
+    list: "home",
+  },
+  {
+    todo: "Defeat neoliberalism",
+    dueDate: "2021-08-11",
+    completed: false,
+    list: "inbox",
+  },
+  {
+    todo: "Become awesome",
+    dueDate: "2021-08-11",
+    completed: true,
+    list: "inbox",
+  },
+  {
+    todo: "Study mycology and develop a start up that uses mycellium networks to sequester C02",
+    dueDate: "2021-08-11",
+    completed: false,
+    list: "inbox",
+  },
+  {
+    todo: "Find a dev job",
+    dueDate: "2021-08-11",
+    completed: false,
+    list: "work",
+  },
+  {
+    todo: "Sign up for 401k",
+    dueDate: "2021-08-11",
+    completed: false,
+    list: "work",
+  },
+  {
+    todo: "Meet Nelson for lunch",
+    dueDate: "2021-08-11",
+    completed: false,
+    list: "work",
+  },
+];
 
 module.exports = dummyTodos;
